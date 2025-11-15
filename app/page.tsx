9e4a187a12d0346563b724c148a258055e3adf9f
GiacomoSchinco <84517547+GiacomoSchinco@users.noreply.github.com>
2025-11-15 10:53:57 +0100

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{ 
+      display: "flex", 
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>Benvenuto nella mia Home!</h1>
+      <p>Questa è una pagina Next.js molto semplice.</p>
+    </main>
+  );
+}
